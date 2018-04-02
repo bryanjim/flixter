ebb8a01ccb559b6db3d11496fcbf5c69da14ffa0
Bryan Jimenez <bryan@roguedevs.io>
2018-04-02 03:32:14 +0000

    Added Student enrollment permission on courses

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,22 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrollment_for_current_course, only: [:show]
   def show
   end
 
   private
 
+  def require_enrollment_for_current_course
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), :alert => 'You must be enrolled to view this lesson'
+    end
+  end
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+
+
 end
